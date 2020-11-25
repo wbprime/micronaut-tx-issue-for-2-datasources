d03a3684f4928e4bca20da06f946d278ecb4d23f
Elvis Wang <mail@wangbo.im>
2020-11-25 15:48:01 +0800

    Change to plain jdbc way to perform op

--- src/main/java/com/example/TheRepository.java
+++ src/main/java/com/example/TheRepository.java
@@ -1,35 +1,52 @@
 package com.example;
 
-import org.jooq.DSLContext;
-
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 @Singleton
 public class TheRepository {
-    private final DSLContext a;
-    private final DSLContext b;
+    private final DataSource a;
+    private final DataSource b;
 
     @Inject
     public TheRepository(
-            @Named("a") final DSLContext a, @Named("b") final DSLContext b) {
+            @Named("a") final DataSource a, @Named("b") final DataSource b) {
         this.a = a;
         this.b = b;
     }
 
     @com.citics.itst.sbl.tasks.dao.Transactional("a")
     public void createTableInA() {
-        a.execute("create table testing_in_a (rid bigint not null auto_increment primary key, name varchar(20) not null)");
+        final DataSource ds = a;
+        try (final Connection c = ds.getConnection(); final Statement stmp = c.createStatement()) {
+            stmp.execute("create table testing_in_a (rid bigint not null auto_increment primary key, name varchar(20) not null)");
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 
     @com.citics.itst.sbl.tasks.dao.Transactional("b")
     public void createTableInB() {
-        b.execute("create table testing_in_b (rid bigint not null auto_increment primary key, name varchar(20) not null)");
+        final DataSource ds = b;
+        try (final Connection c = ds.getConnection(); final Statement stmp = c.createStatement()) {
+            stmp.execute("create table testing_in_b (rid bigint not null auto_increment primary key, name varchar(20) not null)");
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 
     @com.citics.itst.sbl.tasks.dao.Transactional("b")
     public void doSomethingInTransaction() {
-        b.execute("select rid from testing_in_b where rid > 0");
+        final DataSource ds = b;
+        try (final Connection c = ds.getConnection(); final Statement stmp = c.createStatement()) {
+            stmp.execute("select rid from testing_in_b where rid > 0");
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 }
